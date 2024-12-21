1893fd882711cbe89d917ce7206a5fbee48b4629
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-12-22 01:27:03 +0600

    project setup all complete or header part added done

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Header } from "../components/Header"
+
+const MainLayout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+            <Header></Header>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ src/router/router.jsx
@@ -0,0 +1,13 @@
+
+import { createBrowserRouter  } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>
+    }
+])
+
+export default router
+
